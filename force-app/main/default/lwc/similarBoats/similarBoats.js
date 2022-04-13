d824f32dc21f9c01441c3f8853f198bd4ac396a2
Marek Buziak <marek.buziak@gmail.com>
2022-04-13 15:47:27 +0200

    15, 16, 17 in develop - part I

--- force-app/main/default/lwc/similarBoats/similarBoats.js
+++ force-app/main/default/lwc/similarBoats/similarBoats.js
@@ -1,4 +1,6 @@
 // imports
+import { LightningElement, api, wire } from 'lwc';
+import getSimilarBoats from '@salesforce/apex/BoatDataService.getSimilarBoats';
 // import getSimilarBoats
 export default class SimilarBoats extends LightningElement {
     // Private
@@ -6,30 +8,39 @@ export default class SimilarBoats extends LightningElement {
     relatedBoats;
     boatId;
     error;
-    
+
     // public
-    get recordId() {
-        // returns the boatId
-      }
-      set recordId(value) {
-          // sets the boatId value
-          // sets the boatId attribute
-      }
-    
+    @api get recordId() {
+        return this.boatId;
+    }
+    set recordId(value) {
+        this.setAttribute('boatId', value);
+        this.boatId = value;
+        console.log("this.boatId on recordPage:", this.boatId);
+    }
+
     // public
-    similarBy;
-    
+    @api similarBy;
+
     // Wire custom Apex call, using the import named getSimilarBoats
     // Populates the relatedBoats list
-    similarBoats({ error, data }) { }
+    @wire(getSimilarBoats, { boatId: '$boatId', similarBy: '$similarBy'})
+    similarBoats({ error, data }) {
+        if (data) {
+            this.relatedBoats = data;
+            console.log("this.relatedBoats:", JSON.parse(JSON.stringify(this.relatedBoats)));
+        } else if (error) {
+            this.error = error;
+            console.log('this.error:', this.error);
+        }
+    }
     get getTitle() {
-      return 'Similar boats by ' + this.similarBy;
+        return 'Similar boats by ' + this.similarBy;
     }
     get noBoats() {
-      return !(this.relatedBoats && this.relatedBoats.length > 0);
+        return !(this.relatedBoats && this.relatedBoats.length > 0);
     }
-    
+
     // Navigate to record page
     openBoatDetailPage(event) { }
-  }
-  
+}
